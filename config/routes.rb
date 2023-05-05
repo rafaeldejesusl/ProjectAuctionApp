12bc6d1c88cf12d82a9b3343c87d5d425b51be7e
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-05 14:56:59 -0300

    Implementação da página de visualização dos detalhes do item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.create!(name: 'Joao', email: 'joao@email.com', password: 'password',
   cpf: CPF.generate)
 User.create!(name: 'Ana', email: 'ana@leilaodogalpao.com.br', password: 'password',
   cpf: CPF.generate)
-Item.create!(name: 'Cadeira', description: 'Cadeira gamer', image_url: '',
+Item.create!(name: 'Cadeira', description: 'Cadeira gamer', image_url: 'https://m.media-amazon.com/images/I/81fDZaQyoWL.jpg',
   weight: 1200, width: 50, height: 85, depth: 50, category: 'Mobília')
 Item.create!(name: 'Tablet', description: 'Tablet 10" da Samsung', image_url: '',
   weight: 320, width: 15, height: 25, depth: 1, category: 'Eletrônico')
