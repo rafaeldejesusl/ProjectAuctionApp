108ac60f635dacd9944561f2694863b98a31ca44
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-10 20:27:57 -0300

    Implementação de cadastro do lote

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -9,4 +9,21 @@ class LotsController < ApplicationController
   def show
     @lot = Lot.find(params[:id])
   end
+
+  def new
+    @lot = Lot.new()
+  end
+
+  def create
+    lot_params = params.require(:lot).permit(:code, :start_date, :end_date,
+      :minimum_value, :minimal_difference)
+    @lot = Lot.new(lot_params)
+    @lot.created_by = current_user
+    if @lot.save()
+      redirect_to lots_path, notice: 'Lote cadastrado com sucesso'
+    else
+      flash.now[:notice] = 'Lote não cadastrado'
+      render 'new'	
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :items, only: [:index, :show, :new, :create]
-  resources :lots, only: [:index, :show]
+  resources :lots, only: [:index, :show, :new, :create]
 end
