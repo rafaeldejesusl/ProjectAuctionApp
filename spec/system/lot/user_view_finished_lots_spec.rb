9401c8808751af0330cea92e2100988f291e0408
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-12 20:52:48 -0300

    Testes de visualização dos lotes finalizados

---
+++ spec/system/lot/user_view_finished_lots_spec.rb
@@ -0,0 +1,114 @@
+require 'rails_helper'
+
+describe 'Usuário visita tela de lotes finalizados' do
+	it 'quando for administrador' do
+		# Arrange
+    user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+    
+		# Act
+    login_as user
+		visit root_path
+    click_on 'Finalizados'
+		
+		# Assert
+		expect(current_path).to eq root_path
+    expect(page).to have_content 'Não possui autorização'
+	end
+
+  it 'e vê lotes finalizados' do
+		# Arrange
+    user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 1.day.from_now, end_date: 3.day.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+      Lot.create!(code: 'abc123456', start_date: 1.week.from_now, end_date: 3.week.from_now,
+        minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    Bid.create!(value: 50, user: user, lot: lot)
+    
+		# Act
+    login_as user
+		visit root_path
+    click_on 'Finalizados'
+		
+		# Assert
+		expect(page).to have_content 'Lotes Finalizados'
+    expect(page).to have_content 'Lote abc987654'
+    expect(page).to have_content 'Lance Vencedor: 50 R$'
+    expect(page).to have_content 'Usuário Vencedor: Joao'
+    expect(page).not_to have_content 'Lote abc123456'
+	end
+
+  it 'e encerra o lote' do
+		# Arrange
+    user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 1.day.from_now, end_date: 3.day.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    Bid.create!(value: 50, user: user, lot: lot)
+    
+		# Act
+    login_as user
+		visit root_path
+    click_on 'Finalizados'
+    click_on 'Encerrar Lote'
+		
+		# Assert
+    lot = Lot.find(lot.id)
+    expect(lot.status).to eq 'closed'
+    expect(page).not_to have_content 'Lote abc987654'
+	end
+
+  it 'e cancela o lote' do
+		# Arrange
+    user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 1.day.from_now, end_date: 3.day.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    item = Item.create!(name: 'Tablet', description: 'Tablet 10" da Samsung', image_url: '',
+      weight: 320, width: 15, height: 25, depth: 1, category: 'Eletrônico', lot: lot)
+    
+		# Act
+    login_as user
+		visit root_path
+    click_on 'Finalizados'
+    click_on 'Cancelar Lote'
+		
+		# Assert
+    lot = Lot.find(lot.id)
+    expect(lot.status).to eq 'cancelled'
+    expect(page).not_to have_content 'Lote abc987654'
+	end
+
+  it 'e o item volta a ficar disponível' do
+    # Arrange
+    user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 1.day.from_now, end_date: 3.day.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    item = Item.create!(name: 'Tablet', description: 'Tablet 10" da Samsung', image_url: '',
+      weight: 320, width: 15, height: 25, depth: 1, category: 'Eletrônico', lot: lot)
+    
+		# Act
+    login_as user
+		visit root_path
+    click_on 'Finalizados'
+    click_on 'Cancelar Lote'
+    click_on 'Itens'
+		
+		# Assert
+    expect(page).to have_content 'Tablet'
+  end
+end
