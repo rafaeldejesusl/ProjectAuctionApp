9787ff02bdc7af226f8eb2baf174fcc28d34f232
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-11 21:34:11 -0300

    Testes de visualização da página inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,13 +1,86 @@
 require 'rails_helper' # Importação das configurações de test
 
 describe 'Usuário visita tela inicial' do
-	it 'e vê o nome do app' do
+	it 'quando for não for administrador' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+		
+		# Act
+		login_as user
+		visit('/')
+		
+		# Assert
+		expect(current_path).to eq lots_path
+		expect(page).to have_content('Não possui autorização')
+	end
+
+	it 'quando for visitante' do
 		# Arrange
 		
 		# Act
 		visit('/')
 		
 		# Assert
-		expect(page).to have_content('Leilão de Estoque')
+		expect(current_path).to eq root_path
+		expect(page).to have_content('Lotes em Andamento')
+		expect(page).to have_content('Lotes Futuros')
+	end
+
+	it 'quando for usuário' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+		
+		# Act
+		login_as user
+		visit('/')
+		
+		# Assert
+		expect(current_path).to eq root_path
+		expect(page).to have_content('Lotes em Andamento')
+		expect(page).to have_content('Lotes Futuros')
+	end
+
+	it 'e vê lotes em andamento' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+		travel_to 1.week.ago do
+			Lot.create!(code: 'abc123456', start_date: 1.day.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user)
+			Lot.create!(code: 'abc987654', start_date: 1.day.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+		
+		# Act
+		login_as user
+		visit('/')
+		
+		# Assert
+		expect(page).to have_content('Lotes em Andamento')
+		expect(page).not_to have_content('abc123456')
+		expect(page).to have_content('abc987654')
+	end
+
+	it 'e vê lotes futuros' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+		travel_to 1.week.ago do
+			Lot.create!(code: 'abc123456', start_date: 1.day.from_now, end_date: 5.day.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+			Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+		
+		# Act
+		login_as user
+		visit('/')
+		
+		# Assert
+		expect(page).to have_content('Lotes Futuros')
+		expect(page).not_to have_content('abc123456')
+		expect(page).to have_content('abc987654')
 	end
 end
