efd2c2012eb001fa19ae50e89c709886b2ac4c09
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-17 11:39:46 -0300

    Testes de visualização de perguntas

---
+++ spec/system/question/user_view_questions_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+describe 'Usuário visualiza as perguntas' do
+  it 'na tela do leilão' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+    other_lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+      other_lot = Lot.create!(code: 'abc123456', start_date: 2.week.from_now, end_date: 3.week.from_now,
+        minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    question = Question.create!(content: "Quanto é?", user: user, lot: lot)
+    Question.create!(content: "Ser ou não ser?", user: user, lot: other_lot)
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'abc987654'
+		
+		# Assert
+		expect(page).to have_content('Perguntas')
+    expect(page).to have_content('Quanto é?')
+    expect(page).not_to have_content('Ser ou não ser?')
+	end
+
+  it 'quando a visibilidade for verdadeira' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    question = Question.create!(content: "Quanto é?", user: user, lot: lot)
+    Question.create!(content: "Ser ou não ser?", user: user, lot: lot, visible: false)
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'abc987654'
+		
+		# Assert
+		expect(page).to have_content('Perguntas')
+    expect(page).to have_content('Quanto é?')
+    expect(page).not_to have_content('Ser ou não ser?')
+	end
+
+  it 'quando não houver perguntas' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'abc987654'
+		
+		# Assert
+		expect(page).to have_content('Não existem perguntas neste lote')
+	end
+end
