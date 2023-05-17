783b05f44eeb6531dbbe80465b7d4965ff2bdd55
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-17 11:41:30 -0300

    Testes de visualização de perguntas sem respostas

---
+++ spec/system/answer/user_view_unanswered_question_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+describe 'Usuário visualiza as perguntas sem resposta' do
+  it 'quando for admin' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    question = Question.create!(content: "Quanto é?", user: user, lot: lot)
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'Perguntas'
+		
+		# Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Não possui autorização'
+	end
+
+  it 'com sucesso' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+    other_lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+      other_lot = Lot.create!(code: 'abc123456', start_date: 2.week.from_now, end_date: 3.week.from_now,
+        minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    question = Question.create!(content: "Quanto é?", user: user, lot: lot)
+    other_question = Question.create!(content: "Ser ou não ser?", user: user, lot: other_lot)
+    Answer.create!(content: "2 conto", user: user, question: other_question)
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'Perguntas'
+		
+		# Assert
+    expect(page).to have_content 'abc987654'
+    expect(page).to have_content 'Quanto é?'
+    expect(page).not_to have_content 'abc123456'
+	end
+
+  it 'quando não existem perguntas' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'Perguntas'
+		
+		# Assert
+    expect(page).to have_content 'Não existem perguntas'
+    expect(page).not_to have_content 'abc987654'
+	end
+
+	it 'oculta uma pergunta' do
+		# Arrange
+		user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+      cpf: CPF.generate)
+    lot = nil
+		travel_to 1.week.ago do
+			lot = Lot.create!(code: 'abc987654', start_date: 2.week.from_now, end_date: 3.week.from_now,
+				minimum_value: 10, minimal_difference: 5, created_by: user, status: :approved)
+		end
+    question = Question.create!(content: "Quanto é?", user: user, lot: lot)
+		
+		# Act
+		login_as user
+		visit('/')
+    click_on 'Perguntas'
+		click_on 'Ocultar'
+		
+		# Assert
+		expect(page).to have_content 'Pergunta ocultada com sucesso'
+    expect(page).not_to have_content 'Quanto é?'
+	end
+end
