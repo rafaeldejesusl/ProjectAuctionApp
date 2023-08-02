87ac213f8fb7bce6e395629a1a2a78202db0a717
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-08-02 16:52:12 -0300

    Validação de usuário comum para ter lote favorito

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :lot
   validate :not_repeat
+  validate :is_user
 
   private
 
@@ -9,6 +10,13 @@ class Favorite < ApplicationRecord
     favorite = Favorite.where(lot_id: self.lot_id, user_id: self.user_id)
     if !favorite.empty?
       self.errors.add(:lot_id, 'não pode ser repetido.')
+    end
+  end
+
+  def is_user
+    user = User.find(self.user_id)
+    if user.admin
+      self.errors.add(:user_id, 'não pode ser administrador.')
     end
   end
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,11 +4,26 @@ RSpec.describe Favorite, type: :model do
   describe '#valid?' do
 		it 'falso quando usuário e lote forem repetidos' do
 			# Arrange
-			user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+			user = User.create!(name: 'Joao', email: 'joao@email.com.br', password: 'password',
         cpf: CPF.generate)
       lot = Lot.create!(code: 'abc123456', start_date: 1.day.from_now, end_date: 1.week.from_now,
         minimum_value: 10, minimal_difference: 5, created_by: user)
 			Favorite.create!(user: user, lot: lot)
+      favorite = Favorite.new(user: user, lot: lot)
+      
+			# Act
+			result = favorite.valid?
+
+			# Assert
+			expect(result).to eq false
+		end
+
+		it 'falso quando o usuário é administrador' do
+			# Arrange
+			user = User.create!(name: 'Joao', email: 'joao@leilaodogalpao.com.br', password: 'password',
+        cpf: CPF.generate)
+      lot = Lot.create!(code: 'abc123456', start_date: 1.day.from_now, end_date: 1.week.from_now,
+        minimum_value: 10, minimal_difference: 5, created_by: user)
       favorite = Favorite.new(user: user, lot: lot)
       
 			# Act
